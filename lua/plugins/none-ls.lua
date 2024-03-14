db18142d909e5871979d49bdaf567eaf6f802b34
Archit Dharod <archi.parchi7@gmail.com>
2024-03-14 22:44:39 +0100

    added prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,10 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.prettier,
+        -- you could add null_Ls.builtins.diagnostics.<language>
+        -- and null_Ls.builtins.formatting.<language>
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     -- Treesitter
     local configs = require("nvim-treesitter.configs")
     configs.setup({
+      auto_install = true,
       ensure_installed = { "lua", "c", "vim", "vimdoc", "query", "javascript", "html", "typescript", "astro" },
       indent = { enable = true },
     })
