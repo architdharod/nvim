e545dd3c94e184070d03f6a1d34c9906d4a16c6d
archit dharod <archit.dharod@claimini.com>
2024-05-06 23:27:11 +0200

    added full path to the lualine status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,12 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup({
-      options = { theme = "dracula" },
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_c = {{'filename', path = 1 }}
+      }
     })
   end,
 }
